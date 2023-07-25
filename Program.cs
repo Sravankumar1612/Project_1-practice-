9946071dd9b9a2de183f6bc7cfa3f6f9b1755359
Sravankumar1612 <139097275+Sravankumar1612@users.noreply.github.com>
2023-07-25 16:03:23 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -1,56 +1,31 @@
-﻿using System;
+using System;
 using System.IO;
 
 class Program
 {
     static void Main()
     {
-        // File path of the text file containing student data
         string filePath = "E:\\Dotnet\\Test File Input For student.txt";
 
-        // Check if the file exists before reading
+        // Check if the file exists
         if (File.Exists(filePath))
         {
-            try
+            // Read all lines from the file
+            string[] lines = File.ReadAllLines(filePath);
+
+            // Display the student data
+            Console.WriteLine("School student details: \n");
+            foreach (string line in lines)
             {
-                // Read all lines from the text file into an array of strings
-                string[] lines = File.ReadAllLines(filePath);
-
-                // Loop through each line (representing a student record)
-                foreach (string line in lines)
-                {
-                    // Split the line using the comma as the delimiter
-                    string[] data = line.Split(',');
-
-                    // Ensure the data array has the required number of fields
-                    if (data.Length == 4)
-                    {
-                        // Extract the individual fields from the data array
-                        string name = data[0];
-                        int age = int.Parse(data[1]);
-                        int rollNumber = int.Parse(data[2]);
-                        string major = data[3];
-
-                        // Display the student data
-                        Console.WriteLine($"Name: {name}, Age: {age}, Roll Number: {rollNumber}, Major: {major}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid data format: " + line);
-                    }
-                }
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine("Error reading the file: " + e.Message);
+                Console.WriteLine(line);
             }
         }
         else
         {
-            Console.WriteLine("The file 'students.txt' does not exist.");
+            Console.WriteLine("Student data file not found!");
         }
 
-        // Wait for user input before closing the console window (optional)
         Console.ReadLine();
+        
     }
 }
